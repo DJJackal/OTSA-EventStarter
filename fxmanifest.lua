42607cdbd82ca4ab9e19c89acdf44d3ae9eae302
DJ Jackal <djjackal14@gmail.com>
2026-09-05 17:18:38 +0930

    Add FiveM resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CK Computers / Off The Street Australia'
+description 'Standalone server-wide event starter for vMenu servers'
+version '1.2.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
